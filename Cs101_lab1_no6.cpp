8a395bfd888b6b0a7655a422bcc899a599a8d779
Brain17 <b.daminov@newuu.uz>
2025-06-06 23:11:21 +0500

    Add files via upload

---
+++ Cs101_lab1_no6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+double d;
+cin>>d;
+const double pi = 3.14159;
+double r=d/2;
+cout << "Area: " << 4*pi*r*r << endl;
+    return 0;
+}
+
+
