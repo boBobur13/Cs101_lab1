8187b2fcebd93d82d849c41383142c5bff1707f9
Brain17 <b.daminov@newuu.uz>
2025-06-06 22:30:02 +0500

    Add files via upload

---
+++ Cs101_lab1_no3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int a;
+ int b;
+ cin>> a >> b;
+ cout<< "Is a < b? " << (a<b)<< endl;
+ cout<< "Is a > b? " << (a>b) << endl;
+ cout<< "Is a == b? " << (a==b) << endl;
+    return 0;
+}
