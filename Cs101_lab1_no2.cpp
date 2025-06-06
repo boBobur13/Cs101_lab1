9ae11eaa04a5e2aacbd41d0e6a51c4857867d424
Brain17 <b.daminov@newuu.uz>
2025-06-06 22:13:18 +0500

    Add files via upload

---
+++ Cs101_lab1_no2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ double x;
+ double y;
+ cin>> x >> y ;
+ double t=(3*(x*x+3))/((y/4.0)+3);
+ cout<< "t=" << t << endl;
+    return 0;
+}
