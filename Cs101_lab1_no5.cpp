bf47c5abc35294d8aa077470d5bab1480a819d36
Brain17 <b.daminov@newuu.uz>
2025-06-06 22:55:11 +0500

    Add files via upload

---
+++ Cs101_lab1_no5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int num;
+cin >> num;
+num=num-1; // or num--;
+num=num*7; // or
+num=num-14;
+num=num/12;
+num=num+3; // or num+=3;
+int r= num % 3;
+cout << "num = " << num << "r = " << r << endl; // or just count << "r= " << r << endl;
+
+    return 0;
+}
