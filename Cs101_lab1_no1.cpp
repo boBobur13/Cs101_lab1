073041bd3240ade306d0cc76a85a9ac121e7d944
Brain17 <b.daminov@newuu.uz>
2025-06-06 21:42:07 +0500

    Add files via upload

---
+++ Cs101_lab1_no1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int value1;
+ int value2;
+ cin>> value1 >> value2;
+ int Sum=value1+value2;
+ cout<< "Sum: " << value1 << " + " << value2 << " = " << Sum<< endl;
+    return 0;
+}
